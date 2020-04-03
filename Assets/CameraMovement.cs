70535ec299732c930d1507ce698ccc3d2f53fca7
ewenedmunds <ewenedmunds@gmail.com>
2020-04-03 16:27:09 +0100

    Basic Parallax and Sun

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject player;
+    public float maxSpeed;
+
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = new Vector3(Mathf.Clamp(Mathf.Lerp(transform.position.x, player.transform.position.x, maxSpeed * Time.deltaTime), minX, maxX), Mathf.Clamp(Mathf.Lerp(transform.position.y, player.transform.position.y, maxSpeed * Time.deltaTime), minY, maxY), transform.position.z);
+    }
+}
--- Assets/VampireMovement.cs
+++ Assets/VampireMovement.cs
@@ -26,7 +26,7 @@ public class VampireMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void LateUpdate()
+    void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
         {
