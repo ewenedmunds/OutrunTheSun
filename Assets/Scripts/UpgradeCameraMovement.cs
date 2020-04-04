1a0a6fe319a74514c35c1675b3d5249cabdafa68
ewenedmunds <ewenedmunds@gmail.com>
2020-04-04 10:28:35 +0100

    Basic Upgrade Scene

---
+++ Assets/Scripts/UpgradeCameraMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UpgradeCameraMovement : MonoBehaviour
+{
+
+    public Transform target;
+    public float moveSpeed;
+
+    public GameObject selector;
+
+    public TextMeshProUGUI upName;
+    public TextMeshProUGUI upDescription;
+    public Button upButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetNewTarget(target.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+        transform.position = new Vector3(transform.position.x, transform.position.y, -10);
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            SetNewTarget(target.GetComponent<UpgradeTile>().leftNeighbour);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            SetNewTarget(target.GetComponent<UpgradeTile>().rightNeighbour);
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            SetNewTarget(target.GetComponent<UpgradeTile>().upNeighbour);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            SetNewTarget(target.GetComponent<UpgradeTile>().downNeighbour);
+        }
+
+        selector.transform.position = target.position;
+    }
+
+    public void SetNewTarget(GameObject newTarget)
+    {
+        target = newTarget.transform;
+
+        upName.text = newTarget.GetComponent<UpgradeTile>().name;
+        upDescription.text = newTarget.GetComponent<UpgradeTile>().description;
+
+        upButton.interactable = false;
+        if (newTarget.GetComponent<UpgradeTile>().isUnlocked == false) { upButton.interactable = true;  }
+
+    }
+}
---
+++ Assets/Scripts/UpgradeTile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeTile : MonoBehaviour
+{
+
+    public GameObject leftNeighbour;
+    public GameObject rightNeighbour;
+    public GameObject downNeighbour;
+    public GameObject upNeighbour;
+
+    public string name;
+    public string description;
+
+    public bool isUnlocked;
+
+    private void OnMouseDown()
+    {
+        Camera.main.transform.parent.GetComponent<UpgradeCameraMovement>().SetNewTarget(gameObject);
+    }
+}
