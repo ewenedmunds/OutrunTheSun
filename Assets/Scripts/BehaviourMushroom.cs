264c34c45daad1f6534cfa01478faf395155303a
ewenedmunds <ewenedmunds@gmail.com>
2020-04-05 13:55:54 +0100

    Mushroom Enemy

---
+++ Assets/Scripts/BehaviourMushroom.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviourMushroom : MonoBehaviour
+{
+
+    public float sporeDelay;
+    public float delayRandomness;
+    private float sporeTimer;
+
+    public GameObject sporeField;
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sporeTimer -= Time.deltaTime;
+        if (sporeTimer <= 0)
+        {
+            sporeTimer = 99;
+            anim.Play("MushroomAttack");
+        }
+    }
+
+    public void SporeAttack()
+    {
+        GameObject newSpores = Instantiate(sporeField);
+        newSpores.transform.position = transform.position;
+
+        sporeTimer = sporeDelay + Random.Range(0f, delayRandomness);
+    }
+
+    public void Die()
+    {
+        anim.Play("MushroomDie");
+    }
+}
