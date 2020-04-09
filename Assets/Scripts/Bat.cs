9e7c91a4defe7ef67727c0d8aae69ad30b079287
ewenedmunds <ewenedmunds@gmail.com>
2020-04-09 12:18:22 +0100

    Visual Changes

---
+++ Assets/Scripts/Bat.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bat : MonoBehaviour
+{
+
+    private bool isSleeping = true;
+
+    private float velX;
+    private float velY;
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isSleeping)
+        {
+            transform.Translate(new Vector3(velX, velY, 0) * Time.deltaTime);
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && isSleeping)
+        {
+            isSleeping = false;
+            velX = Random.Range(-3f, 3f);
+            velY = 7 - Mathf.Abs(velX);
+            anim.Play("BatFly");
+        }
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+}
